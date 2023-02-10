765d965017234e4564c1f7797377f5f28fea4183
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-02-10 21:48:04 +0800

    2/10/23

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque vel
-        fugit tenetur cum laboriosam at recusandae! Voluptate nihil unde autem?
-      </h1>
+      {/* Start Banner */}
+      <Banner />
+      {/* End Banner */}
+
+      {/* Start Container */}
+      <div className="container mx-auto px-12 py-6 bg-white">
+        <div className="px-4 py-3 bg-[#f1f1f1]">
+          <p>
+            <span className="font-bold">Instructions:</span> Click on a card to
+            activate it, and if the activation of two cards match, it will be
+            considered as a point, but if the activation of two cards does not
+            match, the cards will reset. You must complete the puzzle with less
+            turn.
+          </p>
+        </div>
+        <button className="py-2 px-4 bg-[#313131] text-white rounded mt-4">
+          Start Game
+        </button>
+      </div>
+      {/* End Container */}
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import bannerImage from "../images/banner.png";
+
+function Banner() {
+  return (
+    <div className="w-full py-24">
+      <img className="w-1/6 mx-auto pt-6" src={bannerImage} alt="" />
+      <h4 className="text-center text-4xl text-white font-weight font-bold">
+        Pokeflip
+      </h4>
+    </div>
+  );
+}
+
+export default Banner;
