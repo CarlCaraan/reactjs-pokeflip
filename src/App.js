f65e6c2cba1aacec816ef479dc9b8631637c6ffa
Carl Caraan <bannedefused@gmail.com>
2023-02-10 14:22:23 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque vel
+        fugit tenetur cum laboriosam at recusandae! Voluptate nihil unde autem?
+      </h1>
+    </div>
+  );
+}
+
+export default App;
