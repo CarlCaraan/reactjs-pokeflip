e7284d1e932cf91067077a48b00d165dff4f8212
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-02-10 22:01:07 +0800

    2/10/23

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/Banner";
+import pokeballImage from "./images/pokeball.png";
 
 function App() {
   return (
@@ -9,20 +10,23 @@ function App() {
       {/* End Banner */}
 
       {/* Start Container */}
-      <div className="container mx-auto px-12 py-6 bg-white">
-        <div className="px-4 py-3 bg-[#f1f1f1]">
-          <p>
-            <span className="font-bold">Instructions:</span> Click on a card to
-            activate it, and if the activation of two cards match, it will be
-            considered as a point, but if the activation of two cards does not
-            match, the cards will reset. You must complete the puzzle with less
-            turn.
-          </p>
+      <section className="content-section">
+        <div className="container mx-auto px-12 py-6 bg-white">
+          <div className="px-4 py-3 bg-[#f1f1f1]">
+            <p>
+              <span className="font-bold">Instructions:</span> Click on a card
+              to activate it, and if the activation of two cards match, it will
+              be considered as a point, but if the activation of two cards does
+              not match, the cards will reset. You must complete the puzzle with
+              less turn.
+            </p>
+          </div>
+          <button className="py-2 px-4 bg-[#313131] text-white rounded mt-4 flex flex-row gap-2 items-center hover:opacity-90">
+            <img className="w-6" src={pokeballImage} alt={pokeballImage} />
+            <span>Start Game</span>
+          </button>
         </div>
-        <button className="py-2 px-4 bg-[#313131] text-white rounded mt-4">
-          Start Game
-        </button>
-      </div>
+      </section>
       {/* End Container */}
     </div>
   );
