bc2c9a837755a6413f5fe7f9ee02559a176dd9bd
Carl Caraan <bannedefused@gmail.com>
2023-02-16 17:21:38 +0800

    2/16/23

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,25 @@
 import React from "react";
 import bannerImage from "../images/banner.png";
+import brand from "../images/brand.png";
 
 function Banner() {
   return (
     <div className="w-full py-24">
       <img
         className="w-1/2 mx-auto pt-6
-        sm:w-1/2 md:w-2/4 lg:w-1/6"
+        sm:w-1/2 md:w-1/4 lg:1/3 xl:w-1/6"
         src={bannerImage}
         alt={bannerImage}
       />
       <h4 className="text-center text-4xl text-white font-weight font-bold">
         Pokeflip
       </h4>
+      <img
+        className="w-1/2 mx-auto mt-4
+        sm:w-1/2 md:w-1/4 lg:1/3 xl:w-1/6"
+        src={brand}
+        alt={brand}
+      />
     </div>
   );
 }
