ccc447038ef4fead208edcc47510c02fa4cb2ee8
Carl Caraan <bannedefused@gmail.com>
2023-02-16 17:23:47 +0800

    2/16/23

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="text-center text-white pt-12 pb-4">
+    <div className="text-center text-white pt-24 pb-4">
       <span
         className="text-sm 
         md:text-base"
